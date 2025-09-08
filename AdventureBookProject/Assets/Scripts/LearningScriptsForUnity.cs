8999039b959083487c230444de28de9abe6ac33a
TrentDL <trentdestinl@gmail.com>
2025-09-08 18:33:58 -0500

    LearningScriptsForUnity
    updated after ch2

--- AdventureBookProject/Assets/Scripts/LearningScriptsForUnity.cs
+++ AdventureBookProject/Assets/Scripts/LearningScriptsForUnity.cs
@@ -3,14 +3,28 @@ using UnityEngine;
 public class LearningScriptsForUnity : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int CurrentAge = 30;
+    public int AddedAge = 1;
     void Start()
-    {
-        
+
+    { 
+        Debug.Log(30 + 1); Debug.Log(CurrentAge + 1);
+
+        ComputeAge();
     }
 
     // Update is called once per frame
+
+    /// <summary>
+    /// The secret sauce
+    /// </summary>
+    void ComputeAge()
+    {
+        Debug.Log(CurrentAge + AddedAge);
+    }
     void Update()
     {
         
+
     }
 }
